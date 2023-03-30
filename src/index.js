e3a25d65c174fd296bb441758c29277db7870fcd
bbriceno89 <127133827+bbriceno89@users.noreply.github.com>
2023-03-30 09:03:12 -0600

    labs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,54 @@
-console.log('Write your code here');
+const title = document.querySelector("#exhibit-title");
+const description = document.querySelector("#exhibit-description");
+const ticketsBought = document.querySelector("#tickets-bought");
+const ticketsBoughtButton = document.querySelector("#buy-tickets-button");
+const comments = document.querySelector("#comments-section");
+const commentForm = document.querySelector("#comment-form");
+const image = document.querySelector("#exhibit-image");
+
+document.addEventListener("DOMContentLoaded", getFirstExhibit);
+commentForm.addEventListener("submit", addComment);
+ticketsBoughtButton.addEventListener("click", increaseTicketCount);
+
+function getFirstExhibit() {
+  return fetch("http://localhost:3000/current-exhibits")
+    .then((res) => res.json())
+    .then((data) => {
+      let firstExhibit = data[0];
+      extractExhibitData(firstExhibit);
+    });
+}
+
+function extractExhibitData(exhibit) {
+  title.innerHTML = exhibit.title;
+  description.innerHTML = exhibit.description;
+  ticketsBought.innerHTML = exhibit.tickets_bought;
+  image.src = exhibit.image;
+
+  exhibit.comments.forEach((comment) => {
+    appendComment(comment);
+  });
+}
+
+function appendComment(comment) {
+  let currentComment = document.createElement("p");
+  currentComment.innerHTML = comment;
+  comments.appendChild(currentComment);
+}
+
+function addComment(event) {
+  event.preventDefault();
+  appendComment(event.target[0].value);
+}
+
+function increaseTicketCount(event) {
+  event.preventDefault();
+  console.log(ticketsBought.innerHTML);
+  let currentCount = ticketsBought.innerHTML.split(" ")[0];
+  console.log(currentCount);
+  currentCount++;
+  let newCount = currentCount + " Tickets Bought";
+  console.log(newCount);
+  ticketsBought.innerHTML = newCount;
+}
+
